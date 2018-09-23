6f6fd79cc8f770549d1193b1beadbe4e210695a5
Tarik Kdiry <tarik.kdiry@gmail.com>
2018-09-23 11:48:47 -0400

    included error handling

--- hw_04a.py
+++ hw_04a.py
@@ -2,6 +2,7 @@ import requests
 import json
 from sys import getsizeof
 import pprint
+import os
 
 userInput = input("Username: ")
 
@@ -13,13 +14,20 @@ def gitHubFunction(userInput):
     else:
         repoName = requests.get("https://api.github.com/users/" + userInput + "/repos")
         data = repoName.json()
-        
-        for i in data:
-            name = i["name"]
-            commits = requests.get("https://api.github.com/users/" + userInput + "/" + name + "/commits")
-            commits_data = len(commits.json())
-            # print("Repo:" + name + "Number of commits: " + )
-            print("Repo:" + name + "Number of commits: " + str(commits_data))
+        # print(data)
+        try:
+            for i in data:
+                name = i["name"]
+                commits = requests.get("https://api.github.com/users/" + userInput + "/" + name + "/commits")
+                # commits_data = len(commits.json())
+                commits_data = commits.json()
+                print("Repo:" + name + " Number of commits: " + str(commits_data))
+        except:
+            print("================================================================")
+            print("     YOU HAVE EXCEEDED YOUR API RATE LIMIT. SEE BELOW")
+            print("================================================================")
+            print(os.system("curl -i https://api.github.com/users/" + userInput))
+
 
 
 print(gitHubFunction(userInput))
