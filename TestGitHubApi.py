81d52c3949902ca53b24b9c52e458ec03225bee4
Tarik Kdiry <tarik.kdiry@gmail.com>
2018-09-23 10:15:22 -0400

    semi-successful build

---
+++ TestGitHubApi.py
@@ -0,0 +1,18 @@
+import unittest
+
+from hw_04a import gitHubFunction
+
+class TestGitHubFunction(unittest.TestCase):
+    def testValidUserInput1(self):
+        self.assertEqual(gitHubFunction(1), 'The input username is not valid')
+
+    def testValidUserInput2(self):
+        self.assertEqual(gitHubFunction(10), 'The input username is not valid')
+
+    def testValidUserInput3(self):
+        self.assertEqual(gitHubFunction("asdasdas"), 'This account does not have any repositories')
+
+
+if __name__ == '__main__':
+    print('Running Unit Tests')
+    unittest.main()
--- hw_04a.py
+++ hw_04a.py
@@ -8,24 +8,18 @@ userInput = input("Username: ")
 def gitHubFunction(userInput):
     if (isinstance(userInput, str) != True):
         return "The input username is not valid"
-
-    repoName = requests.get("https://api.github.com/users/" + userInput + "/repos")
-    data = repoName.json()
-    
-    for i in data:
-        name = i["name"]
-        print(name)
-
-
-# def gitHubFunction(userInput):
-#     if (isinstance(userInput, str) != True):
-#         return "The input username is not valid"    
-
-#     result = str("https://api.github.com/users/" + userInput + "/repoName")
-#     parsed_result = json.dumps(result)
-
-#     return parsed_result
+    elif((requests.get("https://api.github.com/users/" + userInput + "/repos")).json() == []):
+        return "This account does not have any repositories"
+    else:
+        repoName = requests.get("https://api.github.com/users/" + userInput + "/repos")
+        data = repoName.json()
+        
+        for i in data:
+            name = i["name"]
+            commits = requests.get("https://api.github.com/users/" + userInput + "/" + name + "/commits")
+            commits_data = len(commits.json())
+            # print("Repo:" + name + "Number of commits: " + )
+            print("Repo:" + name + "Number of commits: " + str(commits_data))
 
 
-# print(gitHubFunction(userInput))
 print(gitHubFunction(userInput))
