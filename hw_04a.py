8fb704f6db80d41790b57d1ea973c8700ce27ceb
Tarik Kdiry <tarik.kdiry@gmail.com>
2018-09-23 01:07:15 -0400

    idk

--- hw_04a.py
+++ hw_04a.py
@@ -1,16 +1,31 @@
 import requests
 import json
+from sys import getsizeof
+import pprint
 
 userInput = input("Username: ")
 
 def gitHubFunction(userInput):
     if (isinstance(userInput, str) != True):
         return "The input username is not valid"
-    else:
-        countRepos = "https://api.github.com/users/" + userInput + "/repos"
-        countCommits = "https://api.github.com/users/" + userInput + "/commits"
 
-    return countRepos
+    repoName = requests.get("https://api.github.com/users/" + userInput + "/repos")
+    data = repoName.json()
+    
+    for i in data:
+        name = i["name"]
+        print(name)
 
 
+# def gitHubFunction(userInput):
+#     if (isinstance(userInput, str) != True):
+#         return "The input username is not valid"    
+
+#     result = str("https://api.github.com/users/" + userInput + "/repoName")
+#     parsed_result = json.dumps(result)
+
+#     return parsed_result
+
+
+# print(gitHubFunction(userInput))
 print(gitHubFunction(userInput))
