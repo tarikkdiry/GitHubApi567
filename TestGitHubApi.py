8f23c15dca4b514232849b02225dbd481363343b
Tarik Kdiry <tarik.kdiry@gmail.com>
2018-09-23 23:36:17 -0400

    new tests

--- TestGitHubApi.py
+++ TestGitHubApi.py
@@ -8,18 +8,22 @@ class TestGitHubFunction(unittest.TestCase):
 
     def testValidUserInput2(self):
         self.assertEqual(gitHubFunction(10), 'The input username is not valid')
-
-    # def testValidUserInput3(self):
-    #     self.assertEqual(gitHubFunction("asdasdas"), 'This account does not have any repositories')
+        self.assertEqual(gitHubFunction(3012), 'The input username is not valid')
+    
+    def testValidUserInput3(self):
+        self.assertEqual(gitHubFunction("asdasdas"), 'This account does not have any repositories')
 
     def testValidUserInput4(self):
         self.assertEqual(gitHubFunction(""), 'You must provide a username')
+        self.assertEqual(gitHubFunction([]), 'You must provide a username')
 
-    # def testValidUserInput5(self):
-    #     self.assertTrue(gitHubFunction("tarikkdiry"))
+    def testValidUserInput5(self):
+        self.assertTrue(gitHubFunction("tarikkdiry"))
+        self.assertTrue(gitHubFunction("richkempinski"))
 
     def testValidUserInput6(self):
         self.assertFalse(gitHubFunction("asdasda"))
+        self.assertFalse(gitHubFunction("/.123/.123"))
 
 if __name__ == '__main__':
     print('Running Unit Tests')
